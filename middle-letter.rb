21ccac3ce4abf24487e92e4c8773517e444e870a
Jared Gallardo <jaredgg@gmail.com>
2017-11-02 14:29:44 -0600

    add text styling

--- middle-letter.rb
+++ middle-letter.rb
@@ -1,38 +1,42 @@
-def middle_letter(input)
+require 'pry'
+require 'rainbow'
+require 'rainbow/ext/string'
+require 'highline/import'
 
+$columns = HighLine::SystemExtensions.terminal_size[0].to_i
+$lines = HighLine::SystemExtensions.terminal_size[1].to_i
+$message_length = 60
+
+def middle_letter(input)
   len = input.length
-  # puts "LEN"
-  # puts len
 
   if len % 2 == 0
     char1 = input[(len / 2 - 1).to_i]
     char2 = input[(len / 2).to_i]
 
     puts "Your string was of an even number of characters."
-    puts ("These are middle two characters: #{char1}, #{char2}")
-    # puts "char1"
-    # puts char1
-    # puts "char2"
-    # puts char2
+    puts ("These are the middle two characters: #{char1}, #{char2}")
   else
-    # puts ((len / 2).to_i)
     char1 = input[(len / 2).to_i]
-    # puts "odd char 1"
-    # puts char1
     puts "Your string was of an odd number of characters."
     puts ("This is the middle character: #{char1}")
   end
 
   puts ""
-  puts 'Enter another string, or type q to exit'
+  puts Rainbow("------------------------------------------------------------").bg(:blue)
+  puts Rainbow('          Enter another string, or type q to exit           ').black.bg(:green)
+  puts Rainbow("------------------------------------------------------------").bg(:blue)
+  puts ""
 end
 
 def welcome_message
-  puts "------------------------------------------------------"
-  puts " Enter a string, and I'll return the middle letter(s) "
-  puts '       ( You can enter "q" to exit at any time )      '
-  puts "------------------------------------------------------"
-  puts ""
+  puts Rainbow("------------------------------------------------------------").bg(:blue)
+  puts Rainbow("                                                            ").bg(:blue)
+  puts Rainbow("    Enter a string, and I'll return the middle letter(s)    ").bg(:blue)
+  puts Rainbow('          ( You can enter "q" to exit at any time )         ').bg(:blue)
+  puts Rainbow("                                                            ").bg(:blue)
+  puts Rainbow("------------------------------------------------------------").bg(:blue)
+  puts Rainbow("").bg(:blue)
 end
 
 
@@ -40,6 +44,7 @@ welcome_message()
 
 while true do
   input = gets.strip
+  puts ""
   if input.downcase == 'q'
     puts "Goodbye!"
     break
